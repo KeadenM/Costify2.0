ab179ee6befb75afc19cdea9faa6bfb0a5718023
Eric Miller <e.s.miller13125@gmail.com>
2023-08-01 20:56:06 -0600

    added code to mutations.js

---
+++ costify/src/utils/mutations.js
@@ -0,0 +1,45 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!, $savingsgoal :Float!) {
+    addUser(username: $username, email: $email, password: $password, savingsgoal: $savingsgoal) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_EXPENSE = gql`
+  mutation addExpense($expense: String!, $amount: Float!) {
+    addExpense(expense: $expense, amount: $amount) {
+        _id
+        expense
+        amount
+    }
+  }
+`;
+
+export const ADD_INCOME = gql`
+  mutation addIncome($income: String!, $amount: Float!) {
+    addIncome(income: $income, amount: $amount) {
+        _id
+        income
+        amount
+    }
+  }
+`;
