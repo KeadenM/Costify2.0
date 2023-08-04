44c3e49d41647d09b1976d5447457efd33c17dae
KeadenM <keadenmeacham@gmail.com>
2023-08-03 21:00:59 -0600

    updated Signup.js

--- costify/src/components/Signup.js
+++ costify/src/components/Signup.js
@@ -1,21 +1,44 @@
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
-function FormFloatingBasicExample() {
+function BasicExample() {
   return (
-    <>
-      <FloatingLabel
-        controlId="floatingInput"
-        label="Email address"
-        className="mb-3"
-      >
-        <Form.Control type="email" placeholder="name@example.com" />
-      </FloatingLabel>
-      <FloatingLabel controlId="floatingPassword" label="Password">
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Sign up</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
         <Form.Control type="password" placeholder="Password" />
-      </FloatingLabel>
-    </>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Sign-up
+      </Button>
+      
+       <Form.Group className="mb-3" controlId="formBasicEmail">
+       <Form.Label>Log in</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+      </Form.Group>
+      <Button variant="primary" type="submit">
+       Log-in
+      </Button>
+    </Form>
+    
+    
   );
 }
 
-export default FormFloatingBasicExample;
+export default BasicExample;
