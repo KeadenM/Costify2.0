6cf06412540e8e87191a8c21061b6b33e74739f3
Daniel Lindquist <dannyl2131@gmail.com>
2023-08-05 15:41:39 -0600

    added form functionality

--- costify/src/utils/mutations.js
+++ costify/src/utils/mutations.js
@@ -13,7 +13,7 @@ export const LOGIN_USER = gql`
 `;
 
 export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!, $savingsgoal :Float!) {
+  mutation addUser($username: String!, $email: String!, $password: String!, $savingsgoal :Float!, $income: Float!) {
     addUser(username: $username, email: $email, password: $password, savingsgoal: $savingsgoal) {
       token
       user {
