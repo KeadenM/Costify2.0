1a2c11781bf977d832174b204b7632dc47ad7684
KeadenM <keadenmeacham@gmail.com>
2023-08-08 19:30:29 -0600

    made main page responsive

--- costify/src/components/Main.js
+++ costify/src/components/Main.js
@@ -3,27 +3,27 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ExpenseList from './Expenses';
 
-const styles = {
-  height: '2000px',
-  border: '2px solid rgba(0, 0, 0, 0.55)'
-}
+// const styles = {
+//   height: '2000px',
+//   border: '2px solid rgba(0, 0, 0, 0.55)',
+// }
 
 function ContainerExample() {
   return (
-    <Container fluid style={{height: '100vh'}}>
+    <Container fluid ={true} style={{height: '100vh'}}>
       <Row>
-        <Col xs={3} style={{height: '100vh'}}>
+        <Col xs={12} sm={12} md={3} style={{height: '50vh'}}>
             <h1>Expense list</h1>
             <ExpenseList></ExpenseList>
         </Col>
 
-        <Col xs={9}>
+        <Col xs={12} sm={12} md={9}>
               <Container fluid>  
                 <Row style={{height: '50vh'}}>
-                    <Col xs={6}>
+                    <Col xs={6} sm={6} md={3} >
                         <h1> Monthly</h1>
                     </Col>
-                    <Col xs={6}>
+                    <Col xs={6} sm={6} md={3}>
                         <h1> Yearly</h1>
                     </Col>
                 </Row>
